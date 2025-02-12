b5935e234f2d0355d49757c59b690811ca196e56
GabrielGS2009 <galicia.leonardo.2024@cbtis-246.edu.mx>
2025-02-12 16:51:31 -0600

    Add files via upload

---
+++ enero.py
@@ -0,0 +1,29 @@
+enero = 31
+febrero =28
+marzo = 31
+abril = 30
+mayo = 31
+junio = 30
+julio = 31
+agosto = 31
+septiembre = 30
+octubre = 31
+noviembre = 30
+diciembre = 31
+
+año = 365
+meses = 12
+
+dia_nac = 11
+mes_nac = enero
+año_nac = 2009
+
+dia_hoy = 5
+mes_hoy = febrero
+año_actual = 2025
+
+años_totales = año_actual - año_nac
+
+dias_vividos = años_totales * año
+
+print(años_totales * meses)
---
+++ pesos.py
@@ -0,0 +1,16 @@
+pesos = input ("cuantos pesos deseas convertir")
+pesos = int(pesos)
+yuan = 2.81
+yen = 0.14
+dolar = 20.47
+euro = 21.26
+libra = 25.46
+
+print ("tus %d equivalen a:", pesos)
+
+
+print ("%d yenes", (pesos * yen))
+print ("%d yuanes",(pesos * yuan))
+print ("%d dolares",(pesos * dolar))
+print ("%d euros",(pesos * euro))
+print ("%d libras",(pesos * libra))
